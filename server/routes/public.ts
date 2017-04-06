8cf0059e3c1ca4a9c705c5c618c32469ee61cb08
konglingkai <konglingkai@ajmide.com>
2017-04-06 10:23:48 +0800

    删除无用的内容。

--- server/routes/public.ts
+++ server/routes/public.ts
@@ -11,20 +11,20 @@ const User = [
   {id:1, username:'bob', password:'secret', email: 'bob@example.com'}
 ];
 
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    User.findOne({ username: username }, function (err, user) {
-      if (err) { return done(err); }
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username.' });
-      }
-      if (!user.validPassword(password)) {
-        return done(null, false, { message: 'Incorrect password.' });
-      }
-      return done(null, user);
-    });
-  }
-));
+// passport.use(new LocalStrategy(
+//   function(username, password, done) {
+//     User.findOne({ username: username }, function (err, user) {
+//       if (err) { return done(err); }
+//       if (!user) {
+//         return done(null, false, { message: 'Incorrect username.' });
+//       }
+//       if (!user.validPassword(password)) {
+//         return done(null, false, { message: 'Incorrect password.' });
+//       }
+//       return done(null, user);
+//     });
+//   }
+// ));
 
 const publicRouter: Router = Router();
 
